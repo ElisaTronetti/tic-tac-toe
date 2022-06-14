d83a9e4058675b5fa39b6749f4c60f06eb9bbfcb
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-06-14 11:17:47 +0200

    Now it is possible to navigate the history of the game

--- src/index.js
+++ src/index.js
@@ -71,12 +71,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true, //we keep track of the turn in the Board
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1); //if we make a move, the future moves are deleted
     const current = history[history.length - 1];
     const squares = current.squares.slice(); //create a copy of the array
     //ignore clicks if someone has won
@@ -88,14 +89,34 @@ class Game extends React.Component {
       history: history.concat([{ //concat does not mutate the original array
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -113,7 +134,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
